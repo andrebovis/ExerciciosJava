9eec1194145e0ff060441915c866a4201bfb58ff
andre bovis <andre_bovis@hotmail.com>
2025-07-25 04:27:49 -0300

    Modulariza o codigo

--- src/Aulas/Aula4.java
+++ src/Aulas/Aula4.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Aula4 {
 
     public static void main(String[] args) {
-
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
@@ -17,33 +16,28 @@ public class Aula4 {
         list.add(10);
         list.add(13);
 
-       
-        double mediaCalculada = calcularMedia(list);
+        realizarAnalisesDaLista(list);
+    }
+
+    public static void realizarAnalisesDaLista(List<Integer> numeros) {
+        System.out.println("--- Resultados da Análise da Lista ---");
+
+        double mediaCalculada = calcularMedia(numeros);
         System.out.println("Média da lista: " + mediaCalculada);
 
-     
-        int menorNumEncontrado = calcularMenorNum(list);
+        int menorNumEncontrado = calcularMenorNum(numeros);
         System.out.println("Menor número na lista: " + menorNumEncontrado);
-        
-        int maiorNumEncontrado = calcularMaiorNum(list);
-        System.out.println("Menor número na lista: " + maiorNumEncontrado);
-        
-        int totalPares = calcularPares(list);
+
+        int maiorNumEncontrado = calcularMaiorNum(numeros);
+        System.out.println("Maior número na lista: " + maiorNumEncontrado);
+
+        int totalPares = calcularPares(numeros);
         System.out.println("Total Pares : " + totalPares);
-        
-        int totalImpares = calcularImpares(list);
+
+        int totalImpares = calcularImpares(numeros);
         System.out.println("Total Impares : " + totalImpares);
 
-        
-
-       
-
-        List<Integer> primosEncontrados = new ArrayList<>();
-        for (Integer numero : list) {
-            if (isPrimo(numero)) {
-                primosEncontrados.add(numero);
-            }
-        }
+        List<Integer> primosEncontrados = encontrarPrimosNaLista(numeros);
         if (primosEncontrados.isEmpty()) {
             System.out.println("Nenhum número primo encontrado na lista.");
         } else {
@@ -51,8 +45,7 @@ public class Aula4 {
         }
     }
 
- 
-    private static boolean isPrimo(int numero) { 
+    private static boolean isPrimo(int numero) {
         if (numero <= 1) {
             return false;
         }
@@ -69,54 +62,66 @@ public class Aula4 {
         for (Integer num : numeros) {
             soma += num;
         }
-        if (numeros.isEmpty()) { 
-            return 0.0; 
+        if (numeros.isEmpty()) {
+            return 0.0;
         }
         return soma / numeros.size();
     }
 
-    
     private static int calcularMenorNum(List<Integer> numeros) {
-        if (numeros.isEmpty()) { 
-            throw new IllegalArgumentException("A lista esta vazia ");
-          
+        if (numeros.isEmpty()) {
+            throw new IllegalArgumentException("A lista está vazia. Não é possível encontrar o menor número.");
         }
-        int menorNum = numeros.get(0); 
-        for (Integer num : numeros) {  
-            if (num < menorNum) {      
-                menorNum = num;        
+        int menorNum = numeros.get(0);
+        for (Integer num : numeros) {
+            if (num < menorNum) {
+                menorNum = num;
             }
         }
         return menorNum;
     }
+
     private static int calcularMaiorNum(List<Integer> numeros) {
-    	if(numeros.isEmpty()) {
-    		throw new IllegalArgumentException("A lista esta vazia ");
-    	}
-    	int maiorNum = numeros.get(0);
-    	for ( Integer num : numeros) {
-    		if (num > maiorNum) {
-    			maiorNum = num;
-    		}
-    	}
-    	return maiorNum;
+        if(numeros.isEmpty()) {
+            throw new IllegalArgumentException("A lista está vazia. Não é possível encontrar o maior número.");
+        }
+        int maiorNum = numeros.get(0);
+        for ( Integer num : numeros) {
+            if (num > maiorNum) {
+                maiorNum = num;
+            }
+        }
+        return maiorNum;
     }
+
     private static int calcularPares(List<Integer> numeros) {
-    	int totalPares = 0;
-    	for ( Integer num :  numeros) {
-    		if (num % 2 ==0) {
-    			totalPares++;
-    		}
-    	}
-    	return totalPares;
+        int totalPares = 0;
+        for ( Integer num : numeros) {
+            if (num % 2 ==0) {
+                totalPares++;
+            }
+        }
+        return totalPares;
     }
+
     private static int calcularImpares(List<Integer> numeros) {
-    	int totalImpares = 0;
-    	for ( Integer num :  numeros) {
-    		if (num % 2 !=0) {
-    			totalImpares++;
-    		}
-    	}
-    	return totalImpares;
+        int totalImpares = 0;
+        for ( Integer num : numeros) {
+            if (num % 2 !=0) {
+                totalImpares++;
+            }
+        }
+        return totalImpares;
+    }
+
+    private static List<Integer> encontrarPrimosNaLista(List<Integer> numeros) {
+        List<Integer> primosEncontrados = new ArrayList<>();
+        for (Integer numero : numeros) {
+            if (isPrimo(numero)) {
+                primosEncontrados.add(numero);
+            }
+        }
+        return primosEncontrados;
     }
 }
+

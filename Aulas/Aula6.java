b41e08a58f63c7e01019f2d6ddadbbad97a05370
andre bovis <andre_bovis@hotmail.com>
2025-08-08 00:52:03 -0300

    Aula6

--- Aulas/Aula6.java
+++ Aulas/Aula6.java
@@ -1,13 +1,41 @@
 package Aulas;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Aula6 {
 
-    public static int listaNumeros (List<Integer> numeros){
-        return 0; 
-    }
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numeros = new ArrayList<>();
+
+        System.out.println("Digite os números (pressione Enter sem digitar nada para finalizar):");
+
+        while (true) {
+            System.out.print("Número: ");
+            String entrada = scanner.nextLine();
+
+            if (entrada.isEmpty()) {
+                break;
+            }
+
+            try {
+                int numero = Integer.parseInt(entrada);
+                numeros.add(numero);
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Por favor, digite um número.");
+            }
+        }
+
+        Collections.sort(numeros);
+
+        System.out.println("\nNúmeros em ordem crescente:");
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+        }
+
+        scanner.close();
     }
 }
